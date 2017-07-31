3fb13b899e93bbe4300b519bbfbd9a554adad7c1
Ling Jin <chloejin525@gmail.com>
2017-07-31 09:27:51 -0400

    changed header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
   def welcome
   end
+
+  def resume
+  	
+  end
+
+  def web
+  	
+  end
+
+  def ios
+  	
+  end
+
+  def contact
+  	
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'pages/welcome'
 
+  get 'resume' => 'pages#resume'
+  get 'web' => 'pages#web'
+  get 'ios' => 'pages#ios'
+  get 'contact' => 'pages#contact'
+
   root 'pages#welcome'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
